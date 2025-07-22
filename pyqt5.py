538634627b6c0816cabc07dc3216789f87407c89
João Vitor Malta <126990089+joaovitormalta@users.noreply.github.com>
2025-07-22 17:46:20 -0300

    aprendendo sobre QtLabel

--- pyqt5.py
+++ pyqt5.py
@@ -1,5 +1,7 @@
 import sys
 from PyQt5.QtWidgets import QApplication, QMainWindow, QLabel
+from PyQt5.QtGui import QFont
+from PyQt5.QtCore import Qt
 
 class MainWindow(QMainWindow):
     def __init__(self):
@@ -8,6 +10,14 @@ class MainWindow(QMainWindow):
         self.setGeometry(100, 100, 500, 500)
 
         label = QLabel("Hello", self)
+        label.setFont(QFont("Arial", 40))
+        label.setGeometry(0, 0, 500, 100)
+        label.setStyleSheet("color: #28eb5f;"
+                            "background-color: #3844c2;"
+                            "font-weight: bold;"
+                            "font-style: italic;"
+                            "text-decoration: underline;")
+        label.setAlignment(Qt.AlignTop | Qt.AlignHCenter)
 
 
 def main():
